fc286b106524a56989f3e3bffbc04002fe49a401
Smash Phil <supersmashphil@gmail.com>
2020-02-07 17:43:20 -0700

    Initial push

---
+++ Source/DropSpot/DropSpot/DropSpot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace DropSpot
+{
+    public class DropSpotIndicator : Building
+    {
+        public DropSpotIndicator()
+        {
+            if(Current.Game.CurrentMap != null)
+            {
+                if(Current.Game.CurrentMap.listerBuildings.allBuildingsColonist.Find(x => x is DropSpotIndicator) is DropSpotIndicator dropSpot)
+                {
+                    dropSpot.Destroy();
+                    Messages.Message("DropSpotDestroyed".Translate(), MessageTypeDefOf.NeutralEvent, false);
+                }
+            }
+        }
+    }
+}
---
+++ Source/DropSpot/DropSpot/DropSpotHarmony.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using OpCodes = System.Reflection.Emit.OpCodes;
+
+namespace DropSpot
+{
+    [StaticConstructorOnStartup]
+    internal static class DropSpotHarmony
+    {
+        static DropSpotHarmony()
+        {
+            var harmony = HarmonyInstance.Create("rimworld.dropspottradeships.smashphil");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            //HarmonyInstance.DEBUG = true;
+
+            harmony.Patch(original: AccessTools.Method(type: typeof(DropCellFinder), name: nameof(DropCellFinder.TradeDropSpot)),
+                prefix: new HarmonyMethod(type: typeof(DropSpotHarmony),
+                name: nameof(CustomDropSpotTradeShips)));
+        }
+
+        public static bool CustomDropSpotTradeShips(Map map, ref IntVec3 __result)
+        {
+            DropSpotIndicator dropSpotIndicator = map.listerBuildings.allBuildingsColonist.Find(x => x is DropSpotIndicator) as DropSpotIndicator;
+            if(dropSpotIndicator != null && !map.roofGrid.Roofed(dropSpotIndicator.Position) && DropSpotHarmony.AnyAdjacentGoodDropSpot(dropSpotIndicator.Position, map, false, false))
+            {
+                IntVec3 dropSpot = dropSpotIndicator.Position;
+                if(!DropCellFinder.TryFindDropSpotNear(dropSpot, map, out IntVec3 singleDropSpot, false, false))
+                {
+                    Log.Error("Could find no good TradeDropSpot near dropCenter " + dropSpot + ". Using a random standable unfogged cell.", false);
+                    singleDropSpot = CellFinderLoose.RandomCellWith((IntVec3 c) => c.Standable(map) && !c.Fogged(map), map, 1000);
+                }
+                __result = singleDropSpot;
+                return false;
+            }
+            return true;
+        }
+
+        private static bool AnyAdjacentGoodDropSpot(IntVec3 c, Map map, bool allowFogged, bool canRoofPunch)
+        {
+            return DropCellFinder.IsGoodDropSpot(c + IntVec3.North, map, allowFogged, canRoofPunch) || DropCellFinder.IsGoodDropSpot(c + IntVec3.East, map, allowFogged, canRoofPunch) || DropCellFinder.IsGoodDropSpot(c + IntVec3.South, map, allowFogged, canRoofPunch) || DropCellFinder.IsGoodDropSpot(c + IntVec3.West, map, allowFogged, canRoofPunch);
+        }
+
+        public static readonly Texture2D ColorWheel = ContentFinder<Texture2D>.Get("colorwheel", true);
+    }
+}
